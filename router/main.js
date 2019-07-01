0e6c4cebc588a59192ee753e7d4886f5f16ef023
cynic991 <48243968+cynic991@users.noreply.github.com>
2019-07-01 20:03:10 +0200

    Add files via upload

---
+++ router/main.js
@@ -0,0 +1,7 @@
+﻿exports.findAll = function(req, res) {
+    res.send([{casa: "Galles", ospite: "Inghilterra"}, {casa: "Italia", ospite: "Irlanda"}]);
+};
+ 
+exports.findById = function(req, res) {
+    res.send({id:req.params.id, casa: "l'oste", ospite: "l'ospite"});
+};
